3f56d2b92baad981f225812a891fbd7285e778d6
root <root@localhost.localdomain>
2020-08-12 14:17:13 +0100

    One

---
+++ main.py
@@ -0,0 +1,18 @@
+
+pathToFile = './contacts.html' 
+
+with open(pathToFile, encoding='utf-8') as f:
+	lines = tuple(l for l in f.readlines())
+	
+	with open('csvToConvert.csv', "w", encoding="utf-8") as outputFile: 
+		#outputFile.write('nome, telefono\n')
+		
+		for i,l in enumerate(lines):
+			
+			if '<div class="name bold">' in l and '<div class="details_entry details">' in lines[i+4]:
+				tel = lines[i+5].replace(' ','')
+				
+				if tel.startswith('00234'):
+					outputFile.write(lines[i+1].replace('\n','').replace(',','').replace(",","").replace('&apos;','') + ', ' + tel.replace('00234', '0'))		
+				else:
+					outputFile.write(lines[i+1].replace('\n','').replace(',','').replace(",","").replace('&apos;','') + ', ' + tel)
